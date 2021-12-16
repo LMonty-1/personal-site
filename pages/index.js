05b1b7d348ea3bf56f39f0dcbccfcacf1cd0028f
Westmonty <lumontgomery@westmont.edu>
2021-12-16 01:39:08 -0800

    more work

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import profilePic from '../public/forst.jpg';
 
 export default function Home() {
-  return (
-    <div>
-      
-    </div>
-  )
+	return (
+		<div>
+			<div style={{ textAlign: 'center', height: '10vh', overflow: 'hidden' }}>
+				<Image src={profilePic} alt="i really like milk" />
+			</div>
+			<div style={{ textAlign: 'center' }}>
+				<h1> basic title </h1>
+			</div>
+		</div>
+	);
 }
