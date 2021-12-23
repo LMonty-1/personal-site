092c865a1be0a83898fa1d69b9af12bffeeeb0fe
LMonty-1 <lumontgomery@westmont.edu>
2021-12-23 15:39:50 -0800

    catch up

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import profilePic from '../public/forst.jpg';
+import bannerPic from '../public/forst.jpg';
 
 export default function Home() {
 	return (
 		<div>
 			<div style={{ textAlign: 'center', height: '10vh', overflow: 'hidden' }}>
-				<Image src={profilePic} alt="i really like milk" />
+				<Image src={bannerPic} alt="i really like milk" />
 			</div>
 			<div style={{ textAlign: 'center' }}>
 				<h1> basic title </h1>
