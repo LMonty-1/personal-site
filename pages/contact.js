f1e05429a8b04fc815dbdd46046ff99f649da0fa
LMonty-1 <lumontgomery@westmont.edu>
2022-01-03 23:27:28 -0800

    contact text

--- pages/contact.js
+++ pages/contact.js
@@ -7,6 +7,13 @@ function ContactPage() {
 			<div class="text-blue-600">
 				<h1> The Contact Page! </h1>
 			</div>
+			<div>
+				<ul>
+					<li> Email: Lumontgomery@westmont.edu</li>
+					<li> Github: LMonty-1 </li>
+					<li> something else: can be found here </li>
+				</ul>
+			</div>
 		</>
 	);
 }
