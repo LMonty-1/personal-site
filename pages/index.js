7b2756351c90edd8d90f652d5409230f0a9f9886
LMonty-1 <lumontgomery@westmont.edu>
2022-01-03 23:25:41 -0800

    homepage text

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,17 @@ function HomePage() {
 			<div class="text-blue-600">
 				<h1> The Home Page! </h1>
 			</div>
+			<div>
+				<p>
+					{' '}
+					Welcome to the Site! If you're seeing this, we're still under
+					constructions. Know that there will be more coming soon! In place of
+					this text, I will tell you all about the cool projects and experience
+					I've had. I will then tell you to check out the Pages tab if you're
+					interested in specifics, the About tab if you want a quick overview,
+					and the Contact tab if you want any of my contact information. Cheers!
+				</p>
+			</div>
 		</>
 	);
 }
