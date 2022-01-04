547c1345dc1c3d89708c364ad0c819e54aed8aac
LMonty-1 <lumontgomery@westmont.edu>
2022-01-03 23:32:28 -0800

    about text

--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,16 @@ function AboutPage() {
 		<>
 			<Navbar />
 			<div class="text-blue-600">
-				<h1> The About Page! </h1>
+				<h1>The About Page!</h1>
+			</div>
+			<div>
+				<p>
+					Hey there! I'm Luke Montgomery, but you've probably heard me go by
+					Monty. I'm a computer science student at Westmont college and I'm
+					looking to be a software engineer coming out of college. I enjoy
+					learning about esoteric programming languages and anything security
+					related.
+				</p>
 			</div>
 		</>
 	);
