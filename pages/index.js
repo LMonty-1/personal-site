96e11af7af343ec388fb61757d1d43d71830bdcc
preet <preet@pop-os.localdomain>
2023-02-26 16:13:31 -0800

    revamp

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,65 @@
-//montyand.me/ or lmonty.com/
-import Navbar from '../components/Navbar';
+// pages/index.js
+import Link from 'next/link';
 
-function HomePage() {
+const HomePage = () => {
 	return (
-		<>
-			<Navbar />
-			<div class="text-blue-600">
-				<h1> The Home Page! </h1>
-			</div>
-			<div>
-				<p>
-					Welcome to the Site! If you're seeing this, we're still under
-					constructions. Know that there will be more coming soon! In place of
-					this text, I will tell you all about the cool projects and experience
-					I've had. I will then tell you to check out the Pages tab if you're
-					interested in specifics, the About tab if you want a quick overview,
-					and the Contact tab if you want any of my contact information. Cheers!
-				</p>
-			</div>
-		</>
+		<div className="bg-my-gray-100 min-h-screen">
+			{/* Navigation */}
+			<nav className="bg-white shadow-lg">
+				<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+					<div className="flex justify-between h-16">
+						<div className="flex-shrink-0 flex items-center">
+							<img className="h-8 w-8" src="/favicon.ico" alt="Logo" />
+							<span className="ml-2 font-bold">Next.js Blog</span>
+						</div>
+						<div className="flex justify-start">
+							<ul className="flex-1 flex items-center justify-start space-x-2">
+								<li>
+									<Link href="/">
+										<a className="link mx-2">Home</a>
+									</Link>
+								</li>
+								<li>
+									<Link href="/articles">
+										<a className="link mx-2">Articles</a>
+									</Link>
+								</li>
+								<li>
+									<Link href="/contact">
+										<a className="link mx-2">Github</a>
+									</Link>
+								</li>
+								<li>
+									<Link href="/contact">
+										<a className="link mx-2">Linkedin</a>
+									</Link>
+								</li>
+								<li>
+									<Link href="/contact">
+										<a className="link mx-2">Contact</a>
+									</Link>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</nav>
+
+			{/* Main content */}
+			<main>
+				<div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+					<div className="px-4 py-6 sm:px-0">
+						<h1 className="text-4xl font-bold text-my-gray-900 mb-4">
+							Welcome to My Site
+						</h1>
+						<p className="text-lg text-my-gray-700">
+							This is a demo site built with Next.js and Tailwind CSS. Feel free to explore and learn how it was created!
+						</p>
+					</div>
+				</div>
+			</main>
+		</div>
 	);
-}
+};
 
 export default HomePage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,32 @@
+// tailwind.config.js
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-	content: [
-		'./pages/**/*.{js,ts,jsx,tsx}',
-		'./components/**/*.{js,ts,jsx,tsx}',
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'my-gray': {
+          '100': '#f7fafc',
+          '900': '#1a202c',
+        },
+      },
+      maxWidth: {
+        '7xl': '112rem',
+      },
+      spacing: {
+        '72': '18rem',
+      },
+      fontFamily: {
+        sans: ['Inter var', 'ui-sans-serif', 'system-ui'],
+      },
+      customUtilities: {
+        link: {
+          base: 'inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-900 hover:text-gray-700',
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
 };
