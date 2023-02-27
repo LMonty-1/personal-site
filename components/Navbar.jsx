c072a27a89f82d7321239bc3c15aba53228d09bf
preet <preet@pop-os.localdomain>
2023-02-26 16:49:42 -0800

    componentize navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,25 +1,48 @@
-//import 'Navbar.css'
 import Link from 'next/link';
-import { Fragment } from 'react';
+import Image from 'next/image';
 
-export const Navbar = (props) => {
+const Navbar = () => {
 	return (
-		<div>
-			<div>
-				<Link href="/">
-					<button>Home</button>
-				</Link>
-				<Link href="/feed">
-					<button>Feed</button>
-				</Link>
-				<Link href="/about">
-					<button>About</button>
-				</Link>
-				<Link href="/contact">
-					<button>Contact</button>
-				</Link>
+		<nav className="bg-white shadow-lg">
+			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+				<div className="flex justify-between h-16">
+					<div className="flex-shrink-0 flex items-center">
+						<Image width="32em" height="32em" src="/favicon.ico" alt="Logo" />
+						<span className="ml-2 font-bold">Next.js Blog</span>
+					</div>
+					<div className="flex justify-start">
+						<ul className="flex-1 flex items-center justify-start space-x-2">
+							<li>
+								<Link href="/">
+									<a className="link mx-2">Home</a>
+								</Link>
+							</li>
+							<li>
+								<Link href="/articles">
+									<a className="link mx-2">Articles</a>
+								</Link>
+							</li>
+							<li>
+								<Link href="/contact">
+									<a className="link mx-2">Github</a>
+								</Link>
+							</li>
+							<li>
+								<Link href="/contact">
+									<a className="link mx-2">Linkedin</a>
+								</Link>
+							</li>
+							<li>
+								<Link href="/contact">
+									<a className="link mx-2">Contact</a>
+								</Link>
+							</li>
+						</ul>
+					</div>
+				</div>
 			</div>
-		</div>
+		</nav>
 	);
 };
+
 export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,13 @@
 // pages/index.js
 import Link from 'next/link';
 import Image from 'next/image';
+import Navbar from '../components/Navbar';
 
 const HomePage = () => {
 	return (
 		<div className="bg-my-gray-100 min-h-screen">
 			{/* Navigation */}
-			<nav className="bg-white shadow-lg">
-				<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-					<div className="flex justify-between h-16">
-						<div className="flex-shrink-0 flex items-center">
-							<Image className="h-8 w-8" src="/favicon.ico" alt="Logo" />
-							<span className="ml-2 font-bold">Next.js Blog</span>
-						</div>
-						<div className="flex justify-start">
-							<ul className="flex-1 flex items-center justify-start space-x-2">
-								<li>
-									<Link href="/">
-										<a className="link mx-2">Home</a>
-									</Link>
-								</li>
-								<li>
-									<Link href="/articles">
-										<a className="link mx-2">Articles</a>
-									</Link>
-								</li>
-								<li>
-									<Link href="/contact">
-										<a className="link mx-2">Github</a>
-									</Link>
-								</li>
-								<li>
-									<Link href="/contact">
-										<a className="link mx-2">Linkedin</a>
-									</Link>
-								</li>
-								<li>
-									<Link href="/contact">
-										<a className="link mx-2">Contact</a>
-									</Link>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</nav>
+			<Navbar />
 
 			{/* Main content */}
 			<main>
